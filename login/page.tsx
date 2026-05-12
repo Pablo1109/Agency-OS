ae7d912fc605c64e4e560b85249058c53caf413c
Pablo1109 <124175056+Pablo1109@users.noreply.github.com>
2026-05-12 20:34:47 -0300

    Delete login directory

--- login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "./login-form";
-
-export default function LoginPage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-muted/40 px-4 py-10">
-      <LoginForm />
-    </main>
-  );
-}
